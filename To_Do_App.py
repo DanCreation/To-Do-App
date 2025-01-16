56080b19c7e272f512c655d399c76b6552a07a81
DanCreation <danjmorris21@gmail.com>
2025-01-16 19:48:46 +0000

    Modification: Replaced task_data function with a Task class
    This class will contain all the variables for any tasks the user wants to add.
    
    - Has a name variable
    - Has a description variable
    
    Quality Improvements:
    - User can now view their list of tasks due to the new view_tasks function
    - User will not be able to view tasks if no tasks exist
    - User will no longer be able to delete tasks if no tasks exist
    - User will no longer be able to update tasks if no tasks exist
    
    Readability:
    - Changed task class name to Task to match the proper naming convention in Python

--- To_Do_App.py
+++ To_Do_App.py
@@ -1,43 +1,55 @@
 
 lst = []
 
-def task_data(name, description):
-    lst.append({"name": name, "description" : description})
-    print("\nTASKS:\n")
-    for x in range(len(lst)):
-        print("Task", x + 1, ": ", lst[x])
+class Task: 
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f"\nName: {self.name} \nDescription: {self.description}"
+    #def task_data(self):
+        #lst.append("Name: " + self.name + " Description: " + self.description)
 
 def add_task(task_name, task_description):
-    #print("\nTASKS:\n" + task_name + "\n" + task_description)
-    task_data(task_name, task_description)
+    lst.append(task(task_name, task_description))
+
+def view_tasks():
+    print("\nTASKS:")
+    for x in range(len(lst)):
+        print("\nTask "+ str(x + 1) + ":", lst[x])
 
 def update_task(task_number, new_name, new_description):
-    lst[task_number - 1]["name"] = new_name
-    lst[task_number - 1]["description"] = new_description
-    print("\nTASKS:\n")
-    for x in range(len(lst)):
-        print("Task", x + 1, ": ", lst[x])
+    lst[task_number - 1].name = new_name
+    lst[task_number - 1].description = new_description
 
 def delete_task(number):
     lst.pop(number - 1)
-    print("\nTASKS:\n")
-    for x in range(len(lst)):
-        print("Task", x + 1, ": ", lst[x])
 
 def main():
     while(True):
-        command = int(input("Press 1 to Add Task\nPress 2 to Update Task\nPress 3 to Delete Task\nPress 4 to Exit\nCommand: "))
-        if(command == 1):
+        command = input("Press 1 to Add Task\nPress 2 to View Tasks\nPress 3 to Update Task\nPress 4 to Delete Task\nPress 5 to Exit\nCommand: ")
+        if(command == "1"):
             add_task(input("\nTask Name: "), input("Task Description: "))
-        elif(command == 2):
-            update_task(int(input("Task Number: ")), input("New Task Name: "), input("New Task Description: "))
-        elif(command == 3):
-            delete_task(int(input("\nTask Number: ")))
-        elif(command == 4):
+        elif(command == "2"):
+            if(len(lst) == 0):
+                print("\nThere are no tasks to view")
+            else:
+                view_tasks()
+        elif(command == "3"):
+            if(len(lst) == 0):
+                print("\nThere are no tasks to update")
+            else:
+                update_task(int(input("Task Number: ")), input("New Task Name: "), input("New Task Description: "))
+        elif(command == "4"):
+            if(len(lst) == 0):
+                print("\nThere are no tasks to delete")
+            else:
+                delete_task(int(input("\nTask Number: ")))
+        elif(command == "5"):
             return False
         else:
-            print("Error")
+            print("\nError")
         print("")
-
 
 main()
