38d215f5df794e1e823c7e9952546c7c419dc5af
DanCreation <danjmorris21@gmail.com>
2025-01-18 19:41:19 +0000

    Features: - Users can now prioritise tasks by their tags - Users can now search tasks by name, description or tag (feature is not fully implemented yet)

--- To_Do_App.py
+++ To_Do_App.py
@@ -1,6 +1,9 @@
 
 lst = []
-
+lst_work = []
+lst_personal = []
+lst_urgent = []
+lst_none = []
 
 class Task: 
     def __init__(self, name, description, tag, status):
@@ -59,6 +62,15 @@ def tag(tag):
     return tag
 
 
+def task_number():
+    while(True):
+        task_num = input("\nTask Number: ")
+        if(task_num.isdigit() and int(task_num) > 0 and int(task_num) <= len(lst)):
+            return False
+        else:
+            print("\nError\n")
+
+
 def add_task(task_name, task_description, task_tag, task_status):
     task_tag = tag(task_tag)
     task_status = status(task_status)
@@ -71,15 +83,151 @@ def view_tasks():
         print("\nTask "+ str(x + 1) + ":", lst[x])
 
 
-def update_task(task_number, new_name, new_description, new_status):
-    new_status = status(new_status)
-    lst[task_number - 1].name = new_name
-    lst[task_number - 1].description = new_description
-    lst[task_number - 1].status = new_status
+def update_task(task_num, new_name, new_description, new_tag, new_status):
+    lst[task_num - 1].name = new_name
+    lst[task_num - 1].description = new_description
+    lst[task_num - 1].tag = tag(new_tag)
+    lst[task_num - 1].status = status(new_status)
 
 
 def delete_task(number):
     lst.pop(number - 1)
+
+
+def prioritise_tasks():
+    for x in range(len(lst)):
+        if(lst[x].tag == "Work"):
+            lst_work.append(lst[x])
+        elif(lst[x].tag == "Personal"):
+            lst_personal.append(lst[x])
+        elif(lst[x].tag == "Urgent"):
+            lst_urgent.append(lst[x])
+        else:
+            lst_none.append(lst[x])
+
+    while(True):
+        priority = input("\nPress 1 for Work Priority\n"
+                         "Press 2 for Personal Priority\n"
+                         "Press 3 for Urgent Priority\n"
+                         "Press 4 for None Priority\n"
+                         "Command: ")
+        if(priority == "1"):
+            for x in range(len(lst_work)):
+                lst[x] = lst_work[x]
+            for x in range(len(lst_personal)):
+                lst[x + len(lst_work)] = lst_personal[x]
+            for x in range(len(lst_urgent)):
+                lst[x + len(lst_work) + len(lst_personal)] = lst_urgent[x]
+            for x in range(len(lst_none)):
+                lst[x + len(lst_work) + len(lst_personal) + len(lst_urgent)] = lst_none[x]
+            lst_work.clear()
+            lst_personal.clear()
+            lst_urgent.clear()
+            lst_none.clear()
+            break
+
+        elif(priority == "2"):
+            for x in range(len(lst_personal)):
+                lst[x] = lst_personal[x]
+            for x in range(len(lst_urgent)):
+                lst[x + len(lst_personal)] = lst_urgent[x]
+            for x in range(len(lst_none)):
+                lst[x + len(lst_personal) + len(lst_urgent)] = lst_none[x]
+            for x in range(len(lst_work)):
+                lst[x + len(lst_personal) + len(lst_urgent) + len(lst_none)] = lst_work[x]
+            lst_work.clear()
+            lst_personal.clear()
+            lst_urgent.clear()
+            lst_none.clear()
+            break
+
+        elif(priority == "3"):
+            for x in range(len(lst_urgent)):
+                lst[x] = lst_urgent[x]
+            for x in range(len(lst_none)):
+                lst[x + len(lst_urgent)] = lst_none[x]
+            for x in range(len(lst_work)):
+                lst[x + len(lst_urgent) + len(lst_none)] = lst_work[x]
+            for x in range(len(lst_personal)):
+                lst[x + len(lst_urgent) + len(lst_none) + len(lst_work)] = lst_personal[x]
+            lst_work.clear()
+            lst_personal.clear()
+            lst_urgent.clear()
+            lst_none.clear()
+            break
+
+        elif(priority == "4"):
+            for x in range(len(lst_none)):
+                lst[x] = lst_none[x]
+            for x in range(len(lst_work)):
+                lst[x + len(lst_none)] = lst_work[x]
+            for x in range(len(lst_personal)):
+                lst[x + len(lst_none) + len(lst_work)] = lst_personal[x]
+            for x in range(len(lst_urgent)):
+                lst[x + len(lst_none) + len(lst_work) + len(lst_personal)] = lst_urgent[x]
+            lst_work.clear()
+            lst_personal.clear()
+            lst_urgent.clear()
+            lst_none.clear()
+            break
+            
+        else:
+            print("\nError\n")
+
+    view_tasks()
+
+
+def search():
+    while(True):
+        task_search = input("\nPress 1 to Search by Name\n"
+                            "Press 2 to Search by Description\n"
+                            "Press 3 to Search by Tag\n"
+                            "Command: ")
+
+        if(task_search == "1"):
+            while(True):
+                search_name = input("\nName: ")
+                num = 0
+                for x in range(len(lst)):
+                    if(search_name == lst[x].name):
+                        temp = lst[x]
+                        lst[x] = lst[num]
+                        lst[num] = temp
+                        num += 1
+                    view_tasks()
+                    return False
+                print("\nThere are no more tasks with that name")
+
+        elif(task_search == "2"):
+            while(True):
+                search_description = input("Description: ")
+                num = 0
+                for x in range(len(lst)):
+                    if(search_description == lst[x].description):
+                        temp = lst[x]
+                        lst[x] = lst[num]
+                        lst[num] = temp
+                        num += 1
+                    view_tasks()
+                    return False
+                print("\nThere are no more tasks with that description")
+
+        elif(task_search == "3"):
+            while(True):
+                search_tag = input("Tag: ")
+                num = 0
+                for x in range(len(lst)):
+                    if(search_tag == lst[x].tag):
+                        temp = lst[x]
+                        lst[x] = lst[num]
+                        lst[num] = temp
+                        num += 1
+                    view_tasks()
+                    return False
+                print("\nThere are no more tasks with that tag")
+
+        else:
+            print("\nError\n")
 
 
 def main():
@@ -88,7 +236,9 @@ def main():
                         "Press 2 to View Tasks\n"
                         "Press 3 to Update Task\n"
                         "Press 4 to Delete Task\n"
-                        "Press 5 to Exit\n"
+                        "Press 5 to Prioritise Tasks\n"
+                        "Press 6 to Search Tasks\n"
+                        "Press 7 to Exit\n"
                         "Command: ")
 
         if(command == "1"):  # Add Task
@@ -111,25 +261,44 @@ def main():
                 print("\nThere are no tasks to view")
             else:
                 view_tasks()
+
         elif(command == "3"):  # Update Task
             if(len(lst) == 0):
                 print("\nThere are no tasks to update")
             else:
-                update_task(int(input("\nTask Number: ")),
-                                input("\nNew Name: "), 
-                                input("New Description: "), 
-                                input("New Status: "
-                                      "Press 1 for Pending \n"
-                                      "        Press 2 for In Progress \n"
-                                      "        Press 3 for Completed \n"
-                                      "        Command: "))
+                task_num = task_number()
+                update_task(task_num,
+                            input("\nNew Name: "), 
+                            input("New Description: "),
+                            input("New Tag: \n"
+                                  "Press 1 for Work Priority\n"
+                                  "Press 2 for Personal Priority\n"
+                                  "Press 3 for Urgent Priority\n"
+                                  "Press 4 for None Priority\n"
+                                  "Command: "),
+                            input("New Status: "
+                                  "Press 1 for Pending \n"
+                                  "        Press 2 for In Progress \n"
+                                  "        Press 3 for Completed \n"
+                                  "        Command: "))
+
         elif(command == "4"):  # Delete Task
             if(len(lst) == 0):
                 print("\nThere are no tasks to delete")
             else:
                 delete_task(int(input("\nTask Number: ")))
-        elif(command == "5"):  # Exit
+
+        elif(command == "5"):  # Prioritise
+            if(len(lst) == 0):
+                print("\nThere are no tasks to prioritise")
+            else:
+                prioritise_tasks()
+        
+        elif(command == "6"):  # Search
+            search()
+        elif(command == "7"):  # Exit
             return False
+
         else:
             print("\nError")
         print("")
