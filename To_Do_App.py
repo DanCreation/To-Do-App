3badeb6f7bb41faa87dd4f953c8ca362c5e550e1
DanCreation <danjmorris21@gmail.com>
2025-01-24 17:09:07 +0000

    User can now add deadlines to their tasks. Currently working on allowing the user to filter their tasks via the deadlines.

--- To_Do_App.py
+++ To_Do_App.py
@@ -2,23 +2,24 @@ import datetime
 
 
 lst = []
-day = 0
-month = 0
-year = 0
-hour = 0
-minute = 0
-deadline = f"{day}-{month}-{year} {hour}:{minute}"
+deadline_lst = []
+
 
 class Task: 
-    def __init__(self, name, description, tag, status, date_time):
+    def __init__(self, name, description, tag, status, deadline, created_date_time, last_modified_date_time):
         self.name = name
         self.description = description
         self.tag = tag
         self.status = status
-        self.date_time = date_time
+        self.deadline = deadline
+        self.created_date_time = created_date_time
+        self.last_modified_date_time = last_modified_date_time
 
     def __str__(self):
-        return f"\nName: {self.name} \nDescription: {self.description} \nTag: {self.tag} \nStatus: {self.status} \nDate/Time: {self.date_time}"
+        return (f"\nName: {self.name} \nDescription: {self.description}" 
+        f"\nTag: {self.tag} \nStatus: {self.status} \nDeadline: {self.deadline}" 
+        f"\nCreated Date/Time: {self.created_date_time}"
+        f"\nLast Modified Date/Time: {self.last_modified_date_time}")
 
 
 def get_task_number():
@@ -40,12 +41,17 @@ def get_description():
     return input("\nDescription: ")
 
 
-def get_deadline():  # Not Finished
-    #deadline = input(f"Deadline (dd-mm-yyyy): {input('Day (dd): ')}-{input('Month (mm): ')}-{input('Year (yyyy): ')}")
-    deadline = datetime.datetime().strptime(f"Deadline (dd-mm-yyyy) (hh:mm): {input('Day (dd): ')}-{input('Month (mm): ')}-{input('Year (yyyy): ')}","%d-%m-%Y %H:%M")
-        
-    return deadline
-
+def get_deadline():
+    deadline_format = "%d-%m-%Y"
+    deadline = input("\nDeadline (dd-mm-yyyy): ")
+    correct_format = True
+    try:
+        correct_format == bool(datetime.datetime.strptime(deadline, deadline_format))
+        return deadline
+    except ValueError:
+        print("\nIncorrect format. Please enter the date as dd-mm-yyyy.")
+        get_deadline()
+    
 
 def get_tag():
     try:
@@ -84,29 +90,34 @@ def get_status():
 
 
 def add_task():
-    lst.append(Task(get_name(), get_description(), get_tag(), get_status(),
-                   datetime.datetime.now().strftime("%d-%m-%Y %H:%M")))
+    lst.append(Task(get_name(), get_description(), get_tag(), get_status(), get_deadline(), 
+                    datetime.datetime.now().strftime("%d-%m-%Y %H:%M"), 
+                    datetime.datetime.now().strftime("%d-%m-%Y %H:%M")))
 
     
 def view_tasks():
     print("\nTASKS:")
-    #print(datetime.datetime.now())
-    #print(datetime.datetime(2025, 4, 18))
     for x in range(len(lst)):
         print("\nTask "+ str(x + 1) + ":", lst[x])
 
 
 def update_task():
     task_num = get_task_number()
+    created = lst[task_num - 1].created_date_time
     lst[task_num - 1] = Task(get_name(), get_description(), get_tag(),
-                            get_status(), datetime.datetime.now())
+                            get_status(), get_deadline(), created,datetime.datetime.now().strftime("%d-%m-%Y %H:%M"))
 
 
 def delete_task(number):
     lst.pop(number - 1)
 
 
-def get_priority(priority, user_input):
+def get_sorted_dates(date_lst):  # NOT FINISHED
+    split_lst = date_lst.split("-")
+    return split_lst[2], split_lst[1], split_lst[0]
+
+
+def get_priority(priority, user_input):  # NOT FINISHED
     num = 0
     if(priority == "name"):
         for x in range(len(lst)):
@@ -129,16 +140,32 @@ def get_priority(priority, user_input):
                 lst[num] = lst[x]
                 lst[x] = temp
                 num += 1
+    elif(priority == "status"):
+        for x in range(len(lst)):
+            if(lst[x].status == user_input):
+                temp = lst[num]
+                lst[num] = lst[x]
+                lst[x] = temp
+                num += 1
+    elif(priority == "deadline"):
+        for x in range(len(lst)):
+            if(lst[x].deadline.find(user_input) != -1):
+                temp = lst[num]
+                lst[num] = lst[x]
+                lst[x] = temp
+                num += 1
     view_tasks()
 
 
-def search():
+def search():  # NOT FINISHED
     try:
         task_search = int(input("\n1. Search by Name\n"
                             "2. Search by Description\n"
                             "3. Search by Tag\n"
+                            "4. Search by Status\n"
+                            "5. Search by Deadline\n"
                             "Command: "))
-        if(task_search <= 0 or task_search > 3):
+        if(task_search <= 0 or task_search > 5):
             raise ValueError("\nInvalid Input.")
         elif(task_search == 1):
             get_priority("name", input("Name: "))
@@ -167,10 +194,52 @@ def search():
             except ValueError as e:
                 print(e)
                 search()
+
+        elif(task_search == 4):
+            try:
+                search_status = int(input("\n1. Pending\n"
+                         "2. In Progress\n"
+                         "3. Completed\n"
+                         "Command: "))
+                if(search_status <= 0 or search_status > 3):
+                    raise ValueError("\nInvalid Input")
+                elif(search_status == 1):
+                    get_priority("status", "Pending")
+                elif(search_status == 2):
+                    get_priority("status", "In Progress")
+                elif(search_status == 3):
+                    get_priority("status", "Completed")
+                return search_status
+            except ValueError as e:
+                print(e)
+                search()
+
+        elif(task_search == 5):  # NOT FINISHED
+            for x in range(len(lst)):
+                deadline_lst.append(lst[x].deadline)
+            deadline_lst.sort(key=get_sorted_dates)
+            try:
+                order = int(input("\n1. Closest Deadlines\n"
+                                  "2. Furthest Deadlines\n"
+                                  "Command: "))
+                if(order <= 0 or order > 2):
+                    raise ValueError("Invalid Input.")
+                elif(order == 1):
+                    for x in range(len(lst)):
+                        for x in range(len(deadline_lst)):
+                            if
+                    #test = list(reversed(deadline_lst))
+                    #print(test)
+                    #get_priority("deadline", input("Deadline: "))
+                elif(order == 2):
+                    pass
+            except ValueError as e:
+                print(e)
+            
     except ValueError as e:
         print(e)
         search()
-            
+          
         
 def main():
     while(True):
@@ -187,7 +256,6 @@ def main():
             match command:
                 case 1:  # ADD TASK
                     add_task()
-                    get_deadline()
                 case 2:  # VIEW TASKS
                     if(len(lst) == 0):
                         print("\nThere are no tasks to view")
